96204e31becf447b2c899376868f6da0458fa6c0
ahsanshah <shahahsan56@gmail.com>
2020-08-02 00:08:24 +0500

    Basic Structure

---
+++ Chat System Server/Chat System Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Chat_System_Server
+{
+    class Program
+    {
+        public static TcpListener tcpListener;
+
+        public static void Main(string[] args)
+        {
+            tcpListener = new TcpListener(2048);
+
+            while (true)
+            {
+                Socket socket = tcpListener.AcceptSocket();
+
+                Byte[] byteMessage=new Byte[250];
+                int res=socket.Receive(byteMessage);
+
+
+                Console.WriteLine(byteMessage.ToString());
+            }
+
+        }
+    }
+}
